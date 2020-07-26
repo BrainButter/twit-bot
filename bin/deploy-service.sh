02756107a7b40e0c646a9a64379b8ec1c13977de
Brain <brianjameslawler@hotmail.com>
2020-07-26 13:55:35 -0400

    root duplicated

--- bin/deploy-service.sh
+++ bin/deploy-service.sh
@@ -1,4 +1,4 @@
-cp ~/root/twit-bot/bin/twitter-bot-server.service /etc/systemd/system/twitter-bot-server.service 
+cp ~/twit-bot/bin/twitter-bot-server.service /etc/systemd/system/twitter-bot-server.service 
 systemctl enable twitter-bot-server.service
 systemctl reload-or-restart twitter-bot-server.service
 systemctl status twitter-bot-server.service
