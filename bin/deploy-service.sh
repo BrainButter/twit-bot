63da5436aa402bb3a92889f14b6ef5f397e64207
Brain <brianjameslawler@hotmail.com>
2020-07-26 14:37:42 -0400

    Update deploy-service.sh

--- bin/deploy-service.sh
+++ bin/deploy-service.sh
@@ -1,3 +1,5 @@
+cd ~/twit-bot/
+npm install
 cp ~/twit-bot/bin/twitter-bot-server.service /etc/systemd/system/twitter-bot-server.service
 systemctl daemon-reload
 systemctl try-reload-or-restart twitter-bot-server.service
