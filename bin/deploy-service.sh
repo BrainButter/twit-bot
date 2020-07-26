40ada103dff22b377e54ad499415223a35726da0
Brain <brianjameslawler@hotmail.com>
2020-07-26 13:52:18 -0400

    Update deploy-service.sh

--- bin/deploy-service.sh
+++ bin/deploy-service.sh
@@ -1,4 +1,4 @@
 cp ~/root/twit-bot/bin/twitter-bot-server.service /etc/systemd/system/twitter-bot-server.service 
 systemctl enable twitter-bot-server.service
-systemctl reload-or-restart application.service
-systemctl status application.service
+systemctl reload-or-restart twitter-bot-server.service
+systemctl status twitter-bot-server.service
